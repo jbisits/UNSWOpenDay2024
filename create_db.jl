b897e792e44298b443c62a9e817ce7749b1d8d64
jbisits <jbisits@gmail.com>
2024-09-06 20:29:16 +1000

    FIx alignment

--- create_db.jl
+++ create_db.jl
@@ -57,7 +57,7 @@ end
 # ╔═╡ c2ea0a8a-b024-42b5-9ead-91c6b43eeb31
 begin
 	sst_cell = PlutoRunner.currently_running_cell_id[] |> string
-	local_sst = LocalResource(sst, :autoplay => "", :loop => "", :width => 1500, :height => 500)
+	local_sst = LocalResource(sst, :autoplay => "", :loop => "", :width => 1500, :height => 800)
 end
 
 # ╔═╡ 6e661452-36f7-4261-9e1e-3e562fdbed0d
@@ -70,20 +70,19 @@ begin
 			<h1 align="center", style="font-size: 80px"> Ocean uptake and trasport of heat</h1>
 			"""
 			PlutoUI.ExperimentalLayout.hbox([
-					PlutoUI.ExperimentalLayout.vbox([
 			md"""
 			
 			The ocean absorbs more than 90% of the Earth’s excess heat due to human-made global warming. Understanding how this heat is distributed globally is vital for assessing long-term impacts such as sea-level rise, marine heat waves, and potential shifts in large-scale ocean circulation patterns. Here we show examples of how this heat is distributed globally and explore the critical role of vertical mixing.
 			
 			Surface waters of the ocean carry the majority of the Earth’s heat budget, making sea surface temperature (SST) a valuable indicator of how heat is distributed around the globe. Most solar energy is absorbed around the equator, warming tropical waters, which are then redistributed through atmospheric and oceanic circulation.
+			A key component of ocean circulation is *mixing* which can increase both the lateral and vertical transportat of heat and salt.
+			Lateral mixing refers to mixing that occurs along surfaces of constant density whereas vertical mixing is across these constant density surfaces.
 			
 			For more information about our group visit the [Climate data and dynamics lab website](https://climate-data-dynamics.github.io/climate-data-dynamics-lab-website/).
 			This dashboard and the simulations that produced the animations can be found at `https://github.com/jbisits/UNSWOpenDay2024`.
-			"""], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "em"))
-				PlutoUI.ExperimentalLayout.vbox([	
-			local_sst], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1em"))
-			],
-			style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "5em"))
+			""",
+			local_sst], 
+			style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1em"))
 		],
 		style=Dict("background" => "border-radius" => "50px", "padding" => "20px", "margin" => "20px", "font-size" => "45px"))
 end
@@ -91,25 +90,25 @@ end
 # ╔═╡ 61b91bdf-ead5-4aa1-a4d5-1e1cda8c50cd
 begin
 	dc_cell = PlutoRunner.currently_running_cell_id[] |> string
-	local_dc = LocalResource(dc, :autoplay => "", :loop => "", :width => 1000, :height => 500)
+	local_dc = LocalResource(dc, :autoplay => "", :loop => "", :width => 1100, :height => 500)
 end
 
 # ╔═╡ bdcfcd1b-6973-4c41-b1d5-08a2ec877e46
 begin
 	convection_cell = PlutoRunner.currently_running_cell_id[] |> string
-	local_convection = LocalResource(convection, :autoplay => "", :loop => "", :width => 1000, :height => 600)
+	local_convection = LocalResource(convection, :autoplay => "", :loop => "", :width => 1000, :height => 800)
 end
 
 # ╔═╡ 46039523-887d-4df6-9be7-170c929fcf72
 begin
 	convection_tracers_cell = PlutoRunner.currently_running_cell_id[] |> string
-	local_convection_tracers = LocalResource(convection_tracers, :autoplay => "", :loop => "", :width => 1000, :height => 1000)
+	local_convection_tracers = LocalResource(convection_tracers, :autoplay => "", :loop => "", :width => 1000, :height => 800)
 end
 
 # ╔═╡ 981b3dc4-f4d7-4466-a0ff-18d1d4938c24
 begin
 	cabbeling_cell = PlutoRunner.currently_running_cell_id[] |> string
-	local_cabbeling = LocalResource(cabbeling, :autoplay => "", :loop => "", :width => 1000, :height => 500)
+	local_cabbeling = LocalResource(cabbeling, :autoplay => "", :loop => "", :width => 1000, :height => 800)
 end
 
 # ╔═╡ f9594b4f-37f9-4741-887c-901873c7e4be
@@ -126,7 +125,7 @@ begin
 					""",
 							PlutoUI.ExperimentalLayout.hbox([
 					md""" 
-				In the ocean, the vertical transport of heat and other biogeochemical tracers is **relatively slow** compared to the lateral transport.
+				In the ocean, the vertical transport of heat and other biogeochemical tracers is **relatively slow** compared to the lateral transport [1].
 				The exception to this is *convective mixing*.
 				
 				Convection occurs when dense water forms atop lighter water.
@@ -141,10 +140,18 @@ begin
 					local_convection_tracers,
 					md"""
 					Our model setup means that no heat or salt enter or escape our domain (for those interested this is because our boundary conditions are horizontally periodic and zero flux vertically).
-					Looking at the salinity and temperature, which determine the density, we can see that the mixing occurs until these tracers are homogeneous over the domain.
-					"""], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1em"))
-					]),
-				
+					Looking at the salinity and temperature, which determine the density, we can see that the convection is very active until these tracers are homogeneous over the domain.
+
+					All animations on this dashboard are from simulations built with `Oceananigans.jl` [2].
+					"""], 
+						style=Dict("justify-content" => "center", "align-items" => "top", "gap" => "1em")),
+										md"""
+
+[1] Waterhouse, A. F., and Coauthors, 2014: Global patterns of diapycnal mixing from
+measurements of the turbulent dissipation rate. Journal of Physical Oceanography, 44, 1854–1872, https://doi.org/10.1175/JPO-D-13-0104.1.\
+[2] Ramadhan, A., and Coauthors, 2020: Oceananigans.jl: Fast and friendly geophysical fluid dynamics on gpus. Journal of Open Source Software, 5, 2018, https://doi.org/10.21105/joss.02018.
+				""",
+					], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1em")),
 					PlutoUI.ExperimentalLayout.vbox([
 					html"""
 					<h1 align="center", style="font-size: 60px"> Other convective instabilities</h1>
@@ -161,11 +168,13 @@ begin
 				""",
 						PlutoUI.ExperimentalLayout.hbox([
 				md"""
-				When temperature is weakening the stability of a water column, and there are no external sources of mixing or turbulence, *diffusive convection* occurs.
+				When temperature is weakening the stability of a water column (i.e. when cold water sits atop warm water), and there are no external sources of mixing or turbulence, *diffusive convection* occurs.
 				In this situation we often see *thermohaline staircases* form where well mixed layers are seperated by very sharp changes in density.
 				These staircases have been well observed in the Artic and increase the rate at which heat is transported vertically.
+
+				When warm/salty water ovelies cold/fresh water, a different convective instability called *salt fingering* occurs.
 				""",
-				local_dc], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "0em")),
+				local_dc], style=Dict("justify-content" => "center", "align-items" => "top", "gap" => "0em")),
 				html"""
 				<h1 align="left", style="font-size: 50px"> Cabbbeling induced instability</h1>
 				""",
@@ -176,13 +185,14 @@ begin
 				One such process is cabbeling whereby the mixture of two water parcels with equal density, but with differing salinity and temperature, is *denser* than the parent water parcels.
 				This process means that a water column can be gravitataionlly stable but if mixed an instability forms triggering convection.
 				This is what we see in the simulation below.
-				For more information on this work see the research poster ''Cabbeling's influence on mixing in Direct Numerical Simulations''.
-				"""], style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "0em"))
-					],style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1.5em"))
+				
+				For more information on how non-linear processes influence vertical mixing see the research poster *Cabbeling's influence on mixing in Direct Numerical Simulations*.
+				"""], style=Dict("justify-content" => "center", "align-items" => "top", "gap" => "1em"))
+					],style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "1em"))
 			],
-			style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "5em"))
+			style=Dict("justify-content" => "center", "align-items" => "center", "gap" => "2.5em"))
 		],
-		style=Dict("justify-content" => "center", "background" => "border-radius" => "50px", "padding" => "20px", "margin" => "20px", "font-size" => "45px"))
+		style=Dict("justify-content" => "center", "background" => "border-radius" => "50px", "padding" => "20px", "margin" => "30px", "font-size" => "45px"))
 end
 
 # ╔═╡ 3f51870b-c4b7-405a-9e1e-f35dd70aac8f
